3640e399939d72fa7984f982c2a4378b1e24b69b
Izuchukwu Nwabueze-Umeh <inwabueze25@gmail.com>
2026-04-03 13:25:19 -0500

    File Checker Functions

---
+++ CS Project Spring 2026/fileCheckerFunctions.py
@@ -0,0 +1,64 @@
+# Nwabueze-Umeh Izuchukwu's functions that checks files
+#|
+#|
+#|
+# where 'password' is the user_input
+
+import re
+
+def main():
+    password = "?loloreo77" # for example
+
+    # get a list of passwords in the leaked password files that are in the password from the user
+    leakedPasswordList = []
+    commonPasswords = existingPasswordChecker(password, leakedPasswordList)
+
+    # get a list of the english words in the password
+    englishWordsList = []
+    englishWords = englishWordsChecker(password, englishWordsList)
+
+
+
+# check if a leaked password from the leaked passwor file is in the password
+def existingPasswordChecker(password, matchedList):
+     # regex that matches only if the entire line is alphanumeric
+    pattern = re.compile(r'^[a-zA-Z0-9]+$')
+    
+    with open('rockyou.txt', 'r', encoding = "latin-1", errors = "ignore") as f:
+        for line in f:
+            cleanLine = line.strip()
+            if not cleanLine:  # skip empty lines
+                continue
+                
+            # check if line is purely alphanumeric
+            if pattern.match(cleanLine):
+
+                # check if the line is in the password
+                if cleanLine in password:
+                    matchedList.append(cleanLine)
+                    
+    return matchedList
+
+
+
+# check if an english word is in the password from the english words file 
+def englishWordsChecker(password, matchedList):
+    # regex that matches if the entire line is alphanumeric
+    pattern = re.compile(r"^[a-zA-Z0-9]+$")
+    
+    with open('english_words.txt', 'r', encoding = "latin-1", errors = "ignore") as f:
+        for line in f:
+            cleanLine = line.strip()
+            if not cleanLine:  # skip empty lines
+                continue
+                
+            # check if line is purely alphanumeric
+            if pattern.match(cleanLine):
+                # check if the line is in the password
+                if cleanLine.lower() in password.lower(): # ignore case because of the english words
+                    matchedList.append(cleanLine)
+    
+    return matchedList
+
+if __name__ == "__main__":
+    main()
